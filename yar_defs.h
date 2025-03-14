1454b084c5a275770ccbc5401575ece2d6fddfc2
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2025-03-15 04:45:06 +0700

    update defs

--- yar_defs.h
+++ yar_defs.h
@@ -5,4 +5,28 @@
         const typeof(((type *)0)->member) *__mptr = (ptr); \
         (type *)( (char *)__mptr - offsetof(type, member) );})
 
+
+// List head iteration
+#define list_entry(ptr, type, member) container_of(ptr, type, member)
+
+#define list_for_each(pos, head) \
+	for (pos = (head)->next; \
+	     pos != (head); \
+         pos = pos->next)
+
+#define list_for_each_safe(pos, tmp, head) \
+    for (pos = (head)->next, tmp = pos->next; \
+		 pos != (head); \
+		 pos = tmp, tmp = pos->next)
+         
+#define list_for_each_prev(pos, head) \
+	for (pos = (head)->prev; \
+	     pos != (head); \
+         pos = pos->prev)
+         
+#define list_for_each_prev_safe(pos, tmp, head) \
+    for (pos = (head)->prev, tmp = pos->prev; \
+         pos != (head); \
+         pos = tmp, tmp = pos->prev)
+
 #endif
