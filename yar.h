d98e65c850578d9c59537efaf40f12152d321a81
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2025-04-19 05:56:11 +0700

    commit

--- yar.h
+++ yar.h
@@ -1,13 +1,6 @@
 #ifndef __YAR_H
 #define __YAR_H
 
-#include "yar_token.h"
-
-void runtime_error (char *message, token tok);
-
-void report (int line, char *where, char *message);
-
-void error (int line, char *message);
 
 #endif
 
