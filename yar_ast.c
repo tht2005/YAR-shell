341d937b5fccf4685fd615484a739139642cdc52
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2025-05-30 03:48:42 +0700

    data structure for handling parsing

--- yar_ast.c
+++ yar_ast.c
@@ -1,5 +1,27 @@
 #include "yar_ast.h"
 
+#include "data_structure/string.h"
+
+string_fragment make_string_fragment (int quoted, string value)
+{
+  string_fragment ret;
+  ret.quoted = quoted;
+  ret.value = value;
+  return ret;
+}
+
+string_fragment_list *new_string_fragment_list (string_fragment str_frag)
+{
+  string_fragment_list *list = (string_fragment_list *) malloc (sizeof (string_fragment_list));
+  if (list == NULL) {
+    perror ("Yar: malloc");
+    exit (1);
+  }
+  list->str_frag = str_frag;
+  list->next = NULL;
+  return list;
+}
+
 redirection make_redirection (int type, string file)
 {
   redirection ret;
@@ -7,3 +29,27 @@ redirection make_redirection (int type, string file)
   ret.file = file;
   return ret;
 }
+
+string_list *new_string_list (string str)
+{
+  string_list *list = (string_list *) malloc (sizeof (string_list));
+  if (list == NULL) {
+    perror ("Yar: malloc");
+    exit (1);
+  }
+  list->str = str;
+  list->next = NULL;
+  return list;
+}
+void free_string_list (string_list *ptr)
+{
+  string_list *nxt;
+  while (ptr)
+  {
+    nxt = ptr->next;
+    free_string(ptr->str);
+    free (ptr);
+    ptr = nxt;
+  }
+}
+
--- yar_ast.h
+++ yar_ast.h
@@ -3,6 +3,34 @@
 
 #include "data_structure/string.h"
 
+typedef struct string_fragment {
+
+  int quoted;
+  string value;
+
+} string_fragment;
+
+string_fragment make_string_fragment (int quoted, string value);
+
+typedef struct string_fragment_list {
+
+  string_fragment str_frag;
+  struct string_fragment_list *next;
+
+} string_fragment_list;
+
+string_fragment_list *new_string_fragment_list (string_fragment str_frag);
+
+typedef struct string_list {
+
+  string str;
+  struct string_list *next;
+
+} string_list;
+
+string_list *new_string_list (string str);
+void free_string_list (string_list *ptr);
+
 typedef struct redirection {
 
   int type;
