c01d17ba4b9aece9651691dd67741ea792be6576
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2025-05-30 18:56:56 +0700

    update builtin-command structure

--- builtin_commands/builtin_commands.c
+++ builtin_commands/builtin_commands.c
@@ -1,6 +1,12 @@
 #include "builtin_commands.h"
 
-void builtin_command_register(const char *name, builtin_func_t func_main, callback_t func_pre, callback_t func_post)
-{
+#include <stdio.h>
 
+void builtin_command_register(const char *name,
+                                builtin_type type,
+                                builtin_func_t func_main,
+                                callback_t func_pre,
+                                callback_t func_post)
+{
+    fprintf (stderr, "Builtin command `%s` registered (%s)\n", name, type == SAME_PROCESS ? "same process" : "child process");
 }
--- builtin_commands/builtin_commands.h
+++ builtin_commands/builtin_commands.h
@@ -1,10 +1,17 @@
 #ifndef __YAR_BUILTIN_COMMANDS_H
 #define __YAR_BUILTIN_COMMANDS_H
 
+typedef enum {
+
+  SAME_PROCESS,
+  CHILD_PROCESS,
+
+} builtin_type;
+
 typedef int (*builtin_func_t)(int argc, char **argv);
 typedef void (*callback_t) ();
 
-void builtin_command_register (const char *name, builtin_func_t func_main, callback_t func_pre, callback_t func_post);
+void builtin_command_register (const char *name, builtin_type type, builtin_func_t func_main, callback_t func_pre, callback_t func_post);
 
 #endif 
 
