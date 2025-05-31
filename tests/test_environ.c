955d1d63a23fbd84e10af6c0ecf29be3370aae86
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2025-05-31 23:26:40 +0700

    tu nhien bug??

--- tests/test_environ.c
+++ tests/test_environ.c
@@ -4,13 +4,13 @@
 #include <assert.h>
 #include <string.h>
 
-char *new_string (const char *str) {
+char *newstr (const char *str) {
     return strdup (str);
 }
 
 void _assert (environ_t *env, char *name, char *value)
 {
-    char *res = getenv_nofree (env, new_string(name));
+    char *res = getenv_nofree (env, newstr(name));
     assert (strcmp (res, value) == 0);
     free (res);
 }
@@ -20,13 +20,13 @@ int main ()
 
     environ_t *env = new_environ ();
     assert (environ_size (env) == 0);
-    putenv_nofree (env, new_string ("abc=def"));
+    putenv_nofree (env, newstr ("abc=def"));
     assert (environ_size (env) == 1);
-    putenv_nofree (env, new_string ("PATH=/usr/bin/abc:/usr/bin/zzz"));
+    putenv_nofree (env, newstr ("PATH=/usr/bin/abc:/usr/bin/zzz"));
     assert (environ_size (env) == 2);
-    putenv_nofree (env, new_string ("equal=equal=equal"));
+    putenv_nofree (env, newstr ("equal=equal=equal"));
     assert (environ_size (env) == 3);
-    putenv_nofree (env, new_string ("null="));
+    putenv_nofree (env, newstr ("null="));
     assert (environ_size (env) == 4);
 
     // test getenv_
@@ -36,40 +36,40 @@ int main ()
     // edge case
     _assert (env, "null", "");
     // // not available
-    assert ( getenv_nofree (env, new_string ("varvarvar")) == NULL );
-    assert ( getenv_nofree (env, new_string ("xyz123")) == NULL );
+    assert ( getenv_nofree (env, newstr ("varvarvar")) == NULL );
+    assert ( getenv_nofree (env, newstr ("xyz123")) == NULL );
 
 
     // test putenv_
-    putenv_nofree (env, new_string ("null=abc"));
+    putenv_nofree (env, newstr ("null=abc"));
     _assert (env, "null", "abc");
     assert (environ_size (env) == 4);
-    putenv_nofree (env, new_string ("va12=zk $_ { \\"));
+    putenv_nofree (env, newstr ("va12=zk $_ { \\"));
     _assert (env, "va12", "zk $_ { \\");
     assert (environ_size (env) == 5);
 
     // test setenv_
-    setenv_nofree (env, new_string ("zzz"), new_string("gg"), 1);
+    setenv_nofree (env, newstr ("zzz"), newstr("gg"), 1);
     _assert (env, "zzz", "gg");
     assert (environ_size (env) == 6);
-    setenv_nofree (env, new_string ("zzz"), new_string("ksdjflkdfjsl"), 0);
+    setenv_nofree (env, newstr ("zzz"), newstr("ksdjflkdfjsl"), 0);
     _assert (env, "zzz", "gg");
     assert (environ_size (env) == 6);
-    setenv_nofree (env, new_string ("zzz"), new_string("sxsx"), 1);
+    setenv_nofree (env, newstr ("zzz"), newstr("sxsx"), 1);
     _assert (env, "zzz", "sxsx");
     assert (environ_size (env) == 6);
 
     // test unsetenv_
-    unsetenv_nofree (env, new_string ("fdjsfkdsl"));
+    unsetenv_nofree (env, newstr ("fdjsfkdsl"));
     assert (environ_size (env) == 6);
-    unsetenv_nofree (env, new_string ("PATH"));
+    unsetenv_nofree (env, newstr ("PATH"));
     assert (environ_size (env) == 5);
     _assert (env, "abc", "def");
     _assert (env, "equal", "equal=equal");
     _assert (env, "null", "abc");
     _assert (env, "va12", "zk $_ { \\");
     _assert (env, "zzz", "sxsx");
-    assert (getenv_nofree (env, new_string ("PATH")) == NULL);
+    assert (getenv_nofree (env, newstr ("PATH")) == NULL);
 
     free_environ (env);
 
